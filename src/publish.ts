ad744140a9b0ab2b335e95ec79f1b9bcc95b6d24
GregRos <life.gregr@gmail.com>
2019-06-03 22:31:08 +0300

    Used my shared tslint config, my shared publish script stuff, some formatting fixes

--- src/publish.ts
+++ src/publish.ts
@@ -6,120 +6,31 @@ import mkdirp = require("mkdirp-promise");
 import {createProgram, ModuleKind, ScriptTarget, getPreEmitDiagnostics, flattenDiagnosticMessageText} from "typescript";
 import {RawSourceMap} from "source-map";
 import * as assert from "assert";
+import {retargetSourcemaps} from "retarget-sourcemaps-after-move";
+import sh = require("shelljs");
 
-async function copyFileAsync(src, dest) {
-    return new Promise((rs, rj) => {
-        fs.copyFile(src, dest, err => {
-            if (err) rj(err);
-            rs();
-        })
-    })
+function run() {
+    let pub = ".tmp/publish";
+    let srcRoot = "src/lib";
+    sh.rm("-rf", pub);
+    sh.mkdir("-p", pub);
+    sh.cp("-r", [
+        "package.json",
+        "LICENSE.md",
+        "README.md"
+    ], pub);
+    sh.cp("-r", "dist/lib/.", pub);
+    sh.cp("-r", "src/lib/.", `${pub}/src`);
+    retargetSourcemaps({
+        srcRoot: {
+            old: srcRoot,
+            new: `${pub}/src`
+        },
+        distRoot: {
+            old: "dist/lib",
+            new: ".tmp/publish"
+        },
+        distGlob: "**/*.js"
+    });
 }
-
-async function run() {
-    await execa.shell("rm -rf .tmp/publish/");
-    await execa.shell("mkdir -p .tmp/publish/src");
-
-    let program = createProgram(globby.sync("src/lib/**/*.ts"), {
-        module: ModuleKind.CommonJS,
-        target: ScriptTarget.ES2015,
-        noImplicitAny: false,
-        sourceMap: true,
-        allowUnreachableCode: true,
-        lib: ["lib.es2015.d.ts"],
-        declaration: true,
-        rootDir: "src/lib",
-        outDir: ".tmp/publish",
-    });
-    let res = program.emit()
-    let allDiagnostics =
-        getPreEmitDiagnostics(program)
-        .concat(res.diagnostics);
-
-    allDiagnostics.forEach(diagnostic => {
-        if (diagnostic.file) {
-            let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-                diagnostic.start!
-            );
-            let message = flattenDiagnosticMessageText(
-                diagnostic.messageText,
-                "\n"
-            );
-            console.log(
-                `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
-            );
-        } else {
-            console.log(
-                `${flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
-            );
-        }
-    });
-    let copyExtras = await globby(["package.json", "LICENSE.md", "README.md"]).then(misc => {
-        return Promise.all(misc.map(cur => {
-            return copyFileAsync(cur, path.join(`.tmp/publish/${path.basename(cur)}`));
-        }))
-    });
-
-
-    let copySources = await globby("src/lib/**/*.ts").then(async sources => {
-        ;
-        return Promise.all(sources.map(async cur => {
-            let rel = path.relative("src/lib", cur);
-            let targetFile = path.join(".tmp/publish/src", rel);
-            await mkdirp(path.dirname(targetFile));
-            return copyFileAsync(cur, targetFile);
-        }))
-
-
-    })
-
-    let sourceMaps = await globby(".tmp/publish/**/*.map").then(maps => {
-        return Promise.all(maps.map(async map => {
-            let text = await fs.readFile(map, {encoding: "utf8"});
-            let json = JSON.parse(text) as RawSourceMap;
-            let relToRoot = path.relative(".tmp/publish", map);
-            let sourcePath = path.join(".tmp/publish/src", relToRoot);
-            let tsFile = sourcePath.replace(/\.js\.map/g, ".ts");
-            let x = 1;
-            assert(await fs.exists(tsFile));
-            let relTsFile = path.relative(path.dirname(map), tsFile);
-
-            json.sources = [
-                relTsFile
-            ];
-            let text2 = JSON.stringify(json);
-            await fs.writeFile(map, text2, {encoding: "utf8"});
-
-        }))
-    });
-
-}
-
 run();
-
-//
-// {
-//  "compilerOptions": {
-//    "module": "commonjs",
-//    "target": "es6",
-//    "noImplicitAny": false,
-//    "sourceMap": true,
-//    "allowUnreachableCode": true,
-//    "lib": [
-//      "es6",
-//      "dom",
-//      "es2015.collection"
-//    ],
-//    "declaration": true,
-//    "sourceRoot": "src",
-//    "outDir": ".tmp/publish",
-//    "rootDir" : "src/lib"
-//  },
-//  "include": [
-//    "src/lib/**/*.ts"
-// ],
-// "exclude": [
-//     "dist",
-//     "node_modules"
-// ]
-// }
