cb074f2d5aaaabb763d379bf15093176d01a7c0c
Mika Vilpas <mika.vilpas@barona.fi>
2023-12-11 19:28:44 +0200

    test: the json example has a snapshot test for complex input

--- src/test/examples/json.spec.ts
+++ src/test/examples/json.spec.ts
@@ -1,4 +1,4 @@
-import { pJsonValue } from "../../examples/json";
+import { exampleInput, pJsonValue } from "../../examples/json";
 
 describe("the JSON example", () => {
     describe("a complex example", () => {
@@ -15,5 +15,71 @@ describe("the JSON example", () => {
             const result = parser.parse(successInput);
             expect(result).toBeSuccessful();
         });
+    });
+
+    it("can parse the example input", () => {
+        const result = pJsonValue.parse(exampleInput);
+        expect(result).toMatchInlineSnapshot(`
+            ParjsSuccess {
+              "kind": "OK",
+              "value": JsonObject {
+                "value": [
+                  JsonObjectProperty {
+                    "name": "a",
+                    "value": JsonNumber {
+                      "value": 2,
+                    },
+                  },
+                  JsonObjectProperty {
+                    "name": "b"",
+                    "value": JsonNumber {
+                      "value": 44325,
+                    },
+                  },
+                  JsonObjectProperty {
+                    "name": "z",
+                    "value": JsonString {
+                      "value": "hi!",
+                    },
+                  },
+                  JsonObjectProperty {
+                    "name": "a",
+                    "value": JsonBool {
+                      "value": true,
+                    },
+                  },
+                  JsonObjectProperty {
+                    "name": "array",
+                    "value": JsonArray {
+                      "value": [
+                        JsonString {
+                          "value": "hi",
+                        },
+                        JsonNumber {
+                          "value": 1,
+                        },
+                        JsonObject {
+                          "value": [
+                            JsonObjectProperty {
+                              "name": "a",
+                              "value": JsonString {
+                                "value": "b"",
+                              },
+                            },
+                          ],
+                        },
+                        JsonArray {
+                          "value": [],
+                        },
+                        JsonObject {
+                          "value": [],
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            }
+        `);
     });
 });
