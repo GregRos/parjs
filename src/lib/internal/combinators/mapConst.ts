e616a7fd4c33606e2e60131a15e932969bc529dd
Mika Vilpas <mika.vilpas@barona.fi>
2023-12-20 20:11:35 +0200

    refactor: move mapConst to its own file

---
+++ src/lib/internal/combinators/mapConst.ts
@@ -0,0 +1,10 @@
+import type { ParjsCombinator } from "../parjser";
+import { map } from "./map";
+
+/**
+ * Applies the source parser and yields the constant value `result`.
+ * @param result The constant value to yield.
+ */
+export function mapConst<T>(result: T): ParjsCombinator<unknown, T> {
+    return map(() => result);
+}
