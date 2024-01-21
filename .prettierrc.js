3d671fc21f004069fcad5232836b7cf8128a0ea9
GregRos <life.gregr@gmail.com>
2024-01-21 15:24:54 +0200

    fix: fix #95, add lint-staged, apply prettier to everything

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,13 @@ module.exports = {
     tabWidth: 4,
     arrowParens: "avoid",
     trailingComma: "none",
-    printWidth: 100
+    printWidth: 100,
+    overrides: [
+        {
+            files: "*.{json,yml,yaml}",
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
 };
