c590464a4a8900d49ef1988073b6e671ad3c8e5d
GregRos <life.gregr@gmail.com>
2024-02-11 22:18:17 +0200

    refactor: Refactored case-string to match other refactors

--- src/lib/internal/parsers/case-string.ts
+++ src/lib/internal/parsers/case-string.ts
@@ -10,34 +10,41 @@ import { ResultKind } from "../result";
 import { ParjserBase } from "../parser";
 import type { Parjser } from "..";
 
+class CaseInsensitiveString extends ParjserBase<string> {
+    expecting = `expecting '${this.str}' (case insensitive)`;
+    type = "string";
+    constructor(private str: string) {
+        super();
+    }
+
+    _apply(ps: ParsingState): void {
+        const { position, input } = ps;
+        const { str } = this;
+        if (position + str.length > input.length) {
+            ps.kind = ResultKind.SoftFail;
+            return;
+        }
+        // This should create a StringSlice object instead of actually
+        // copying a whole string.
+        const substr = input.slice(position, position + str.length);
+
+        // Equality test is very very fast.
+        if (substr.toLowerCase() !== str.toLowerCase()) {
+            ps.kind = ResultKind.SoftFail;
+            return;
+        }
+        ps.position += str.length;
+        ps.value = substr;
+        ps.kind = ResultKind.Ok;
+    }
+}
+
 /**
  * Returns a parser that will parse the string `str` insensitive to its case
  * and yield the text that was parsed. If it can't, it will fail softly without
  * consuming input.
  * @param str The string to parse case insensitively.
  */
-export function caseString<T extends string>(str: T): Parjser<T> {
-    return new (class ParseString extends ParjserBase<T> {
-        expecting = `expecting '${str}', case insensitively`;
-        type = "string";
-        _apply(ps: ParsingState): void {
-            const { position, input } = ps;
-            if (position + str.length > input.length) {
-                ps.kind = ResultKind.SoftFail;
-                return;
-            }
-            // This should create a StringSlice object instead of actually
-            // copying a whole string.
-            const substr = input.slice(position, position + str.length);
-
-            // Equality test is very very fast.
-            if (substr.toLowerCase() !== str.toLowerCase()) {
-                ps.kind = ResultKind.SoftFail;
-                return;
-            }
-            ps.position += str.length;
-            ps.value = substr;
-            ps.kind = ResultKind.Ok;
-        }
-    })();
+export function caseString(str: string): Parjser<string> {
+    return new CaseInsensitiveString(str);
 }
